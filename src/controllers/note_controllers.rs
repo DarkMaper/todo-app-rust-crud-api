9144addbf1df3ac34ba37194c2dd8cae8e04a6cd
DarkMaper <darkmaper@gmail.com>
2022-04-20 16:04:52 +0200

    chore: Deleted unused imports and added update and delete methods

--- src/controllers/note_controllers.rs
+++ src/controllers/note_controllers.rs
@@ -1,6 +1,4 @@
-use rocket::response::{Debug, status::Created};
-use rocket::serde::{Serialize, Deserialize};
-use rocket::http::Status;
+use rocket::response::{Debug, status::{Created, NotFound}};
 use rocket::serde::json::Json;
 
 use crate::diesel::prelude::*;
@@ -45,4 +43,74 @@ pub async fn get_notes(db: Db, token: Token) -> Result<Json<Vec<Note>>> {
     }).await?;
 
     Ok(Json(notes))
+}
+
+#[get("/<id>")]
+pub async fn get_note_info(db: Db, token: Token, id: i32) -> Result<Json<Note>, NotFound<String>> {
+    let user = token.0.claims.sub;
+
+    let note = db.run(move |conn| {
+        notes::table
+            .filter(notes::user.eq(user).and(notes::id.eq(id)))
+            .first(conn)
+    }).await.map(Json);
+
+    match note {
+        Ok(data) => {
+            Ok(data)
+        },
+        Err(_) => {
+            Err(NotFound("No existe la nota".to_string()))
+        }
+    }
+}
+
+#[put("/update/<id>", data = "<note_data>")]
+pub async fn update_note(db: Db, token: Token, id: i32, note_data: Json<Note>) -> Result<Created<Json<Note>>, NotFound<String>> {
+
+    let user = token.0.claims.sub;
+    let note_values = note_data.clone();
+
+    let affected = db.run(move |conn| {
+        diesel::update(notes::table)
+            .filter(notes::user.eq(user).and(notes::id.eq(id)))
+            .set((notes::title.eq(note_values.title), notes::body.eq(note_values.body)))
+            .execute(conn)
+    }).await.ok();
+
+    match affected {
+        Some(data) => {
+            if data > 0 {
+                return Ok(Created::new("/").body(note_data));
+            }
+            
+            Err(NotFound("No existe ninguna nota con ese ID".to_string()))
+        },
+        None => {
+            Err(NotFound("No existe ninguna nota con ese ID".to_string()))
+        }
+    }
+}
+
+#[delete("/delete/<id>")]
+pub async fn delete_note(db: Db, token: Token, id: i32) -> Result<Option<()>, NotFound<String>> {
+    let user = token.0.claims.sub;
+
+    let affected = db.run(move |conn| {
+        diesel::delete(notes::table)
+            .filter(notes::user.eq(user).and(notes::id.eq(id)))
+            .execute(conn)
+    }).await.ok();
+
+    match affected {
+        Some(data) => {
+            if data > 0 { 
+                return Ok(Some(()));
+            }
+
+            Err(NotFound("No existe ninguna nota con ese id".to_string()))
+            
+        },
+        None => Err(NotFound("No existe ninguna nota con ese id".to_string()))
+    }
 }
