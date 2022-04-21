ecd97df1335608b6d38ebef410ce9f75b16f2845
DarkMaper <darkmaper@gmail.com>
2022-04-21 16:03:22 +0200

    chore: Models working with uuid instead ID integers

--- migrations/2022-04-18-155357_users/up.sql
+++ migrations/2022-04-18-155357_users/up.sql
@@ -1,5 +1,7 @@
 -- Your SQL goes here
 CREATE TABLE IF NOT EXISTS users(
-    email VARCHAR(30) NOT NULL PRIMARY KEY,
-    password VARCHAR(60) NOT NULL
+    id VARCHAR(36) NOT NULL,
+    email VARCHAR(30) NOT NULL UNIQUE,
+    password VARCHAR(60) NOT NULL,
+    PRIMARY KEY (id)
 );
