ba856c759c989d221276e06c9450bbbe2ace2c6e
Luis Orozco <lorozco@petroprix.com>
2022-06-09 10:15:25 +0200

    chore: Added /api in all mounts. Register a unauthorized catcher

--- src/main.rs
+++ src/main.rs
@@ -3,26 +3,33 @@
 #[macro_use] extern crate diesel_migrations;
 #[macro_use] extern crate diesel;
 
+
 pub mod models;
+
 pub mod db;
 pub mod controllers;
 pub mod guards;
 pub mod lib;
+pub mod catchers;
 
 use crate::controllers::user_controllers::*;
 use crate::controllers::note_controllers::*;
+
+
+use crate::catchers::*;
 
 #[launch]
 fn rocket() -> _ {
     rocket::build()
     .attach(db::stage())
-    .mount("/auth", routes![signup, signin])
-    .mount("/notes", routes![
+    .mount("/api/auth", routes![signup, signin, refresh_token, logout])
+    .mount("/api/notes", routes![
         create_note, 
         get_notes, 
         get_note_info, 
         update_note,
         delete_note
         ])
+    .register("/", catchers![unauthorized])
     .attach(lib::cors::stage())
 }
