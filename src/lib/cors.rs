bc7278457c3d004dc9d00e07566174b201000599
Luis Orozco <lorozco@petroprix.com>
2022-06-10 11:59:09 +0200

    fix: Added http://localhost:3000 to allow_origins_vec so if the environment variable is not set, don't throw an error

--- src/lib/cors.rs
+++ src/lib/cors.rs
@@ -4,7 +4,8 @@ use std::env;
 
 pub fn stage() -> AdHoc {
     let allow_origins: String = env::var("ALLOWED_ORIGINS").unwrap();
-    let allow_origins_vec: Vec<&str> = allow_origins.split(",").collect();
+    let mut allow_origins_vec: Vec<&str> = allow_origins.split(",").collect();
+    allow_origins_vec.push("http://localhost:3000");
     let cors = CorsOptions {
         allowed_origins: AllowedOrigins::some_exact(&allow_origins_vec),
         ..Default::default()
